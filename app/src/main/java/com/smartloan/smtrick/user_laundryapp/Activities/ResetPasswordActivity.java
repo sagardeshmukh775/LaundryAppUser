06951d17b9888a48772886b7be1a95ac44f94c35
sagar <sagardeshmukh447@gmail.com>
2019-12-03 17:57:15 +0530

    03.12.2019 update at 5.57

--- app/src/main/java/com/smartloan/smtrick/user_laundryapp/Activities/ResetPasswordActivity.java
+++ app/src/main/java/com/smartloan/smtrick/user_laundryapp/Activities/ResetPasswordActivity.java
@@ -1,23 +1,39 @@
 package com.smartloan.smtrick.user_laundryapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.smartloan.smtrick.user_laundryapp.Models.User;
 import com.smartloan.smtrick.user_laundryapp.R;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
-    private EditText inputNumber,inputName;
+    private EditText inputNumber, inputName;
     private Button btnReset, btnBack;
     private FirebaseAuth auth;
     private ProgressBar progressBar;
+    private DatabaseReference mDatabase;
+    String key;
+    LinearLayout layout_Update;
+    EditText inputPassword;
+    Button btnUpdatePAssword;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +46,12 @@ public class ResetPasswordActivity extends AppCompatActivity {
         btnBack = (Button) findViewById(R.id.btn_back);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
+        inputPassword = (EditText) findViewById(R.id.password);
+        layout_Update = (LinearLayout) findViewById(R.id.layout_update_password);
+        btnUpdatePAssword = (Button) findViewById(R.id.btn_update_password);
+
+        layout_Update.setVisibility(View.GONE);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,7 +59,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
             }
         });
 
-       btnReset.setOnClickListener(new View.OnClickListener() {
+        btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -52,6 +74,57 @@ public class ResetPasswordActivity extends AppCompatActivity {
                     Toast.makeText(getApplication(), "Enter your registered Name", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                progressBar.setVisibility(View.VISIBLE);
+
+                Query query6 = FirebaseDatabase.getInstance().getReference("users")
+                        .orderByChild("number")
+                        .equalTo(number);
+
+                query6.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        for (DataSnapshot Snapshot : dataSnapshot.getChildren()) {
+                            user = Snapshot.getValue(User.class);
+                            if (user != null) {
+                                layout_Update.setVisibility(View.VISIBLE);
+                                key = user.getGeneratedId();
+                                progressBar.setVisibility(View.GONE);
+                            } else {
+                                Toast.makeText(ResetPasswordActivity.this, "Sorry No User Found", Toast.LENGTH_SHORT).show();
+                                layout_Update.setVisibility(View.GONE);
+                                progressBar.setVisibility(View.GONE);
+                            }
+
+//
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+        btnUpdatePAssword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String passwprd = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(passwprd)) {
+                    Toast.makeText(getApplication(), "Enter your Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mDatabase = FirebaseDatabase.getInstance().getReference();
+                mDatabase.child("users").child(key).child("number").setValue(passwprd);
+                Toast.makeText(ResetPasswordActivity.this, "Password Updated", Toast.LENGTH_SHORT).show();
+                Intent intent =new Intent(ResetPasswordActivity.this,LoginActivity.class);
+                startActivity(intent);
 
             }
         });
